67ae3327c7c0b4e2588bfbd1dce4cf1f770f2dab
neeraja <nerys123@gmail.com>
2022-10-18 21:51:07 -0400

    New Classes comitted

---
+++ src/main/java/com/learning1/com/javalearning1/FourOperatorsProgram.java
@@ -0,0 +1,11 @@
+package com.learning1.com.javalearning1;
+
+public class FourOperatorsProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My name is neeraja");
+
+	}
+
+}
